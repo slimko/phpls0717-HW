fb50201d02098f49ba25da974b5e377253db32aa
Slimko <slimko2008@yandex.ru>
2017-07-08 20:25:08 +0300

    ДЗ 1 выполнено

---
+++ HW-01/task-02.php
@@ -0,0 +1,10 @@
+<?php
+
+$NumberOfDrawings = 80;
+$FeltTipPen = 23;
+$Pencil = 23;
+
+$paints = $NumberOfDrawings-$FeltTipPen-$Pencil;
+
+
+print "На школьной выставке $NumberOfDrawings  рисунков. $FeltTipPen из них выполнены фломастерами, $Pencil карандашами, а остальные $paints — красками.";
