906ca29f2408c6455eb5dc0e0a5b2810af831fb9
Slimko <slimko2008@yandex.ru>
2017-07-08 22:43:21 +0300

    ДЗ 1 выполнено

---
+++ HW-01/task-08.php
@@ -0,0 +1,15 @@
+<?php
+
+$str = "Раз Два Три Четыре Пять";
+echo "<p>$str<p>";
+
+$arr = explode(' ', $str);
+echo '<pre>' . print_r($arr, true) . '</pre>';
+
+$length = count($arr);
+while ($length) {
+    $r_array[] = $arr[$length - 1];
+    $length--;
+}
+$newStr = implode(' * ', $r_array);
+echo $newStr;
